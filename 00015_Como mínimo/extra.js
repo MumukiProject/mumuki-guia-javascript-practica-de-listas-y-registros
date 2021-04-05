47e9436e988669bf38ab308a6f8771098635b1bf
gustavo@mumuki.org <gustavo@mumuki.org>
2021-04-05 22:16:14 +0000

    Mumuki Export on 2021-04-05 22:16:14 +0000

--- 00015_Como mínimo/extra.js
+++ 00015_Como mínimo/extra.js
@@ -7,7 +7,7 @@
 function minimo(numeros) /*<elipsis-for-student@*/ {
   return Math.min.apply(null, numeros)
 } /*@elipsis-for-student>*/
-// Retorna el menor valor dentro de una lista de números
+// Retorna el menor valor dentro de una lista de números:
 //
 //  ム minimo([5, 7, 8, 9, 3]) 
 //  => 3
