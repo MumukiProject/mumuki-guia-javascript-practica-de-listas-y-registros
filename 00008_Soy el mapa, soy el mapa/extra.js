52a5d9a19b5f30d5344f6ddeead1a74b629c9d16
gustavo@mumuki.org <gustavo@mumuki.org>
2021-07-16 20:34:15 +0000

    Mumuki Export on 2021-07-16 20:34:15 +0000

--- 00008_Soy el mapa, soy el mapa/extra.js
+++ 00008_Soy el mapa, soy el mapa/extra.js
@@ -1,3 +1,10 @@
-function agregar(lista, elemento) {
-  return lista.push(elemento);
-}
+function agregar(unaLista, unElemento) /*<elipsis-for-student@*/ {
+  return unaLista.push(unElemento);
+} /*@elipsis-for-student>*/
+// Inserta unElemento al final de unaLista.
+// Este es un procedimiento que no retorna nada pero modifica a unaLista:
+//
+//  ム let cancionesFavoritas = ["La colina de la vida", "Zamba por vos"]
+//  ム agregar(cancionesFavoritas, "Seminare")
+//  ム cancionesFavoritas
+//  ["La colina de la vida", "Zamba por vos", "Seminare"]
